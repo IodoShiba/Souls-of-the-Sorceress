74272484dcac5d8f01e55fcae0ef4caea70e5162
LAPTOP-SSQC0SPI\B_tow <1754hogefooo@gmail.com>
2018-11-07 00:23:32 +0900

    仮ステージ、横移動、ジャンプ仮実装した

---
+++ Assets/Scripts/HorizontalMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMove : MonoBehaviour {
+    [SerializeField] float speed;
+    [SerializeField] KeyCode temp_rightKey;
+    [SerializeField] KeyCode temp_leftKey;
+
+    public float Speed
+    {
+        set
+        {
+            speed = value;
+        }
+    }
+
+    private void Update()
+    {
+        float d = 0;
+        if (Input.GetKey(temp_rightKey))
+        {
+            d += speed;
+        }
+        if (Input.GetKey(temp_leftKey))
+        {
+            d -= speed;
+        }
+        gameObject.transform.position += new Vector3(d * Time.deltaTime, 0, 0);
+    }
+
+    void Act()
+    {
+        gameObject.transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    [SerializeField,Range(100,3000)] float power;
+    [SerializeField] KeyCode temp_Key;
+    [SerializeField] GameObject groundSensor;
+    [SerializeField] string groundTagName;
+    private bool jumpable = false;
+
+    // Use this for initialization
+    private void Start()
+    {
+        var c=groundSensor.AddComponent<GroundSensor>();
+        c.setJumpable = this.SetJumpable;
+        c.groundTagName = this.groundTagName;
+        c.enabled = true;
+    }
+    // Update is called once per frame
+    void Update() {
+        if (jumpable&&Input.GetKeyDown(temp_Key))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, power));
+            jumpable = false;
+        }
+    }
+    private void SetJumpable(bool b) { jumpable = b; }
+
+    private class GroundSensor : MonoBehaviour
+    {
+        public System.Action<bool> setJumpable;
+        public string groundTagName;
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == groundTagName) { 
+                setJumpable(true);
+            }
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == groundTagName)
+            {
+                setJumpable(false);
+            }
+        }
+    }
+}
