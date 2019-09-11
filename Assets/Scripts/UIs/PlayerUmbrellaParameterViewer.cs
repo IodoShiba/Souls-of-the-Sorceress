34265e8378363c7ccf6ee6a340fa3d9734e6a9da
iodo_shiba <iodoshiba@gmail.com>
2019-09-11 14:08:00 +0900

    Sarahの傘耐久度メーターを完成させた SarahのパラメータUI完成に伴い仮UIを削除した

--- Assets/Scripts/UIs/PlayerUmbrellaParameterViewer.cs
+++ Assets/Scripts/UIs/PlayerUmbrellaParameterViewer.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class PlayerUmbrellaParameterViewer : UmbrellaParameters.Viewer
 {
-    [SerializeField] List<UnityEngine.UI.Toggle> gaugeElements;
+    [SerializeField] UDPointElement elementPrefab;
 
+    List<UDPointElement> gaugeElements = new List<UDPointElement>();
+
+    private void Start()
+    {
+        gaugeElements.Clear();
+    }
     // Update is called once per frame
     void Update()
     {
+        AdjustElements();
+
         for (int i = 0; i < target.MaxDurability; ++i)
         {
-            gaugeElements[i].isOn = i < target.Durability;
+            gaugeElements[i].Usable = i < target.Durability;
+        }
+    }
+
+    void AdjustElements()
+    {
+        if (gaugeElements.Count < target.MaxDurability)
+        {
+            var selfRTrans = GetComponent<RectTransform>();
+            for(int i = gaugeElements.Count; i < target.MaxDurability; ++i)
+            {
+                UDPointElement ins = Instantiate(elementPrefab, transform.position + Vector3.right * selfRTrans.sizeDelta.x * 2 * i, Quaternion.identity);
+                ins.transform.parent = transform;
+                var rTrans = ins.GetComponent<RectTransform>();
+                ins.SetSize(new Vector2((rTrans.sizeDelta.x / rTrans.sizeDelta.y) * selfRTrans.sizeDelta.y, selfRTrans.sizeDelta.y));
+                gaugeElements.Add(ins);
+            }
+        }
+        else if (gaugeElements.Count > target.MaxDurability)
+        {
+            for (int i = gaugeElements.Count-1; i >= target.MaxDurability; --i)
+            {
+                Destroy(gaugeElements[i].gameObject);
+                gaugeElements.RemoveAt(i);
+            }
         }
     }
 }
---
+++ Assets/UDPointElement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UDPointElement : MonoBehaviour
+{
+    [SerializeField] bool usable;
+    [SerializeField] UnityEngine.UI.Image usableImage;
+    public bool Usable {
+        get => usable;
+        set {
+            usableImage.enabled = usable = value;
+        }
+    }
+
+    RectTransform selfRTrans;
+    RectTransform childRTrans;
+    RectTransform SelfRTrans { get => selfRTrans == null ? (selfRTrans = GetComponent<RectTransform>()) : selfRTrans; }
+    RectTransform ChildRTrans { get => childRTrans == null ? (childRTrans = usableImage.GetComponent<RectTransform>()) : childRTrans; }
+
+    public void SetSize(in Vector2 size)
+    {
+        ChildRTrans.sizeDelta = SelfRTrans.sizeDelta = size;
+        ChildRTrans.localScale = SelfRTrans.localScale = Vector3.one;
+    }
+}
