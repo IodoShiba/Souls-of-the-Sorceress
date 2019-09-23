afa4bd3435122781b3f00e817b9247703cf3a8e7
iodo_shiba <iodoshiba@gmail.com>
2019-09-24 02:58:53 +0900

    背景のスクリプトを修正した 背景を調整した

--- Assets/BackGround.cs
+++ Assets/BackGround.cs
@@ -12,12 +12,12 @@ public class BackGround : MonoBehaviour
         public SpriteRenderer spriteRendererLoopX;
         public SpriteRenderer spriteRendererLoopY;
         public SpriteRenderer spriteRendererLoopXY;
-        [Range(0,1)] public float sizeProportion;
         public float margin;
+        public bool forceSpriteFullyContainCamera;
     }
 
     [SerializeField] BoxCollider2D area;
-    [SerializeField] GameObject monitoringGameObject;
+    //[SerializeField] GameObject monitoringGameObject;
     [SerializeField] List<Layer> layers;
 
     private void FixedUpdate()
@@ -26,19 +26,29 @@ public class BackGround : MonoBehaviour
         if (area == null) { throw new System.ArgumentNullException($"field '{nameof(area)}' cannot be null."); }
         if (area.bounds.size.x < float.Epsilon || area.bounds.size.y < float.Epsilon) { throw new System.ArgumentException($"bound of field '{nameof(area)}' is too small or too thin."); }
 
+        Camera mainCam = Camera.main;
+        Vector3 cameraSizeHalf = new Vector3(mainCam.aspect * mainCam.orthographicSize, mainCam.orthographicSize,0);
+
         for (int i = 0; i < layers.Count; ++i)
         {
-            Vector2 monitObjPos = monitoringGameObject.transform.position;
+            Vector2 monitObjPos = mainCam.transform.position;
             Vector2 posProp = (monitObjPos - (Vector2)area.bounds.center);
-            posProp.x /= area.bounds.size.x / 2;
-            posProp.y /= area.bounds.size.y / 2;
+            Vector3 movableSizeArea = area.bounds.size - 2 * cameraSizeHalf;
+            posProp.x /= movableSizeArea.x / 2;
+            posProp.y /= movableSizeArea.y / 2;
+            Vector3 movableSizeSprite = layers[i].spriteRenderer.bounds.size - 2 * cameraSizeHalf - layers[i].margin * Vector3.one;
+
+            if (layers[i].forceSpriteFullyContainCamera)
+            {
+                posProp.x = Mathf.Clamp(posProp.x, -1, 1);
+                posProp.y = Mathf.Clamp(posProp.y, -1, 1);
+            }
 
             layers[i].spriteRenderer.transform.position = (Vector3)monitObjPos + new Vector3(
-                -layers[i].spriteRenderer.bounds.size.x / 2 * layers[i].sizeProportion * posProp.x,
-                -layers[i].spriteRenderer.bounds.size.y / 2 * layers[i].sizeProportion * posProp.y,
+                -movableSizeSprite.x / 2 * posProp.x,
+                -movableSizeSprite.y / 2 * posProp.y,
                 layers[i].spriteRenderer.transform.position.z
                 );
         }
-
     }
 }
