2fbf57d8ca99a1e5007c39dbd8a5fbc7d52437e5
iodo_shiba <iodoshiba@gmail.com>
2020-03-28 01:37:08 +0900

    ボム敵を実装した

---
+++ Assets/BombAI.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActorBomb
+{
+    public class BombAI : AI
+    {
+        [SerializeField] float maxChaseDistance;
+        [SerializeField] float minChaseDistance;
+        [SerializeField] float timeToIgnite;
+        [SerializeField] Color _def;
+        [SerializeField] Color _igniting;
+        [SerializeField] SpriteRenderer spriteRenderer;
+
+        Actor player;
+        Actor Player { get => player == null ? (player = ActorManager.PlayerActor) : player; }
+
+        float detectingTime;
+
+        bool detectingPlayer;
+        bool DetectingPlayer
+        { 
+            get => detectingPlayer;
+            set 
+            {
+                if (detectingPlayer && !value) { detectingTime = 0; }
+                detectingPlayer = value;
+            } 
+        }
+
+        public bool Ignite
+        {
+            get => detectingTime > timeToIgnite;
+        }
+
+        int moveDir;
+        public int MoveDir { get => moveDir; }
+
+        public override void Decide()
+        {
+            float dx = Player.transform.position.x - transform.position.x;
+            
+            if(minChaseDistance < Mathf.Abs(dx) && Mathf.Abs(dx) < maxChaseDistance)
+            {
+                moveDir = System.Math.Sign(dx);
+            }
+
+            if (DetectingPlayer)
+            {
+                detectingTime += Time.deltaTime;
+            }
+
+            spriteRenderer.color = Color.Lerp(_def, _igniting, detectingTime / timeToIgnite);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == TagName.player)
+            {
+                Debug.Log("Detecting Player...");
+                DetectingPlayer = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if(collision.tag == TagName.player)
+            {
+                DetectingPlayer = false;
+            }
+        }
+    }
+}
