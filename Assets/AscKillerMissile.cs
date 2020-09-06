fe428fc221a2edaf076366613f026920e2f1f23b
iodo_shiba <iodoshiba@gmail.com>
2020-09-07 01:09:13 +0900

    大砲・キラーの実装途中

---
+++ Assets/AscKillerMissile.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AscKillerMissile : FightActorStateConector
+{
+    [SerializeField] MissileDefault missileDefault;
+    [SerializeField] Detecting detecting;
+    [SerializeField] MisileSmashed smashed;
+
+    public override SmashedState Smashed => smashed;
+
+    public override ActorState DefaultState => missileDefault;
+
+    protected override void BuildStateConnection()
+    {
+        base.BuildStateConnection();
+        ConnectStateFromDefault(()=>missileDefault.DidStaightDistancePass(), detecting);
+    }
+
+
+    [System.Serializable]
+    class MissileDefault : DefaultState
+    {
+        [SerializeField] Rigidbody2D rigidbody;
+        [SerializeField] float distanceGoStraight;
+        Vector2 initPos;
+
+        public bool DidStaightDistancePass() => Vector2.Distance(initPos, GameObject.transform.position) >= distanceGoStraight;
+
+        AscKillerMissile connectorMissile;
+        AscKillerMissile ConnectorMissile 
+        {
+            get => connectorMissile == null ? (connectorMissile = (AscKillerMissile)Connector) : connectorMissile;
+        }
+
+        protected override void OnInitialize()
+        {
+            initPos = GameObject.transform.position;
+        }
+    }
+
+    [System.Serializable]
+    class Detecting : ActorState
+    {
+        [SerializeField] Rigidbody2D rigidbody;
+        [SerializeField] float accel;
+        Vector2 dir;
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            dir = (ActorManager.PlayerActor.transform.position - GameObject.transform.position).normalized;
+            Vector2 v = dir * rigidbody.velocity.magnitude;
+            rigidbody.velocity = v;
+        }
+
+        protected override void OnActive()
+        {
+            base.OnActive();
+            rigidbody.AddForce(dir*(accel*rigidbody.mass));
+        }
+    }
+
+    [System.Serializable]
+    class MisileSmashed : SmashedState 
+    {
+        [SerializeField] Rigidbody2D rigidbody;
+        [SerializeField] float gravityScale;
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            rigidbody.gravityScale = gravityScale;
+        }
+    }
+}
---
+++ Assets/TriggerPublisher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerPublisher : MonoBehaviour
+{
+    [System.Serializable] class UnityEvent_Collider2D : UnityEvent<Collider2D> {}
+
+    [SerializeField, TagField] string tag;
+    [SerializeField] UnityEvent_Collider2D onEnter;
+    [SerializeField] UnityEvent_Collider2D onExit;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(string.IsNullOrEmpty(tag) || other.CompareTag(tag))
+        {
+            onEnter.Invoke(other);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(string.IsNullOrEmpty(tag) || other.CompareTag(tag))
+        {
+            onExit.Invoke(other);
+        }
+    }
+}
