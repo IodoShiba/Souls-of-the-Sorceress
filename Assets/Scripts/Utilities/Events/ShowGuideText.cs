f4878ac6fd1d91b7e2b75a7ef19899de1fdec823
iodo_shiba <iodoshiba@gmail.com>
2020-11-23 15:02:09 +0900

    チュートリアルガイドを仮組した

---
+++ Assets/Scripts/Utilities/Events/ShowGuideText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowGuideText : MonoBehaviour
+{
+    [SerializeField, TextArea] string instruction;
+    [SerializeField, TextArea] string description;
+
+    public void Show()
+    {
+        GuideText.MainInstance.Show(instruction, description);
+    }
+
+    public void Hide()
+    {
+        GuideText.MainInstance.Hide();
+    }
+}
