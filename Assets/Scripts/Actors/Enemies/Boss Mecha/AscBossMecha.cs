9c65a09936d88eb36d1777007537df05c9eea493
iodo_shiba <iodoshiba@gmail.com>
2021-06-19 02:41:36 +0900

    続き

--- Assets/Scripts/Actors/Enemies/Boss Mecha/AscBossMecha.cs
+++ Assets/Scripts/Actors/Enemies/Boss Mecha/AscBossMecha.cs
@@ -7,6 +7,8 @@ using System;
 
 public class AscBossMecha : FightActorStateConector
 {
+    [SerializeField] Rect fieldRect;
+
     [SerializeField] BossMechaDefault defaultState;
     [SerializeField] SmashedState smashed;
     [SerializeField] BossMechaDead dead;
@@ -16,6 +18,25 @@ public class AscBossMecha : FightActorStateConector
     public override SmashedState Smashed => smashed;
     public override DeadState Dead => dead;
 
+    protected override void BuildStateConnection()
+    {
+        
+    }
+
+    public void DropBomb() 
+    {
+
+    }
+
+    public void SetFloatingBomb() 
+    {
+        
+    }
+
+    public void InvokeAllBomb()
+    {
+
+    }
 
     [System.Serializable]
     class BossMechaDefault : DefaultState
@@ -26,8 +47,10 @@ public class AscBossMecha : FightActorStateConector
     [System.Serializable]
     class BombingTackle : ActorState
     {
+
         bool ongoing = false;
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        AscBossMecha conector = null; AscBossMecha MechaConnector { get => conector == null ? (conector = Connector as AscBossMecha) : conector; }
 
         protected override bool ShouldCotinue() => ongoing;
         protected override void OnInitialize()
@@ -48,8 +71,8 @@ public class AscBossMecha : FightActorStateConector
         {
             do
             {
-                if(await DroppingBomb(cancellationToken)) { break; }
-                if(await Tackle(cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await DroppingBomb(cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await Tackle(cancellationToken)) { break; }
             } while(false);
 
             ongoing = false;
@@ -87,6 +110,7 @@ public class AscBossMecha : FightActorStateConector
     {
         bool ongoing = false;
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        AscBossMecha conector = null; AscBossMecha MechaConnector { get => conector == null ? (conector = Connector as AscBossMecha) : conector; }
 
         protected override bool ShouldCotinue() => ongoing;
         protected override void OnInitialize()
@@ -107,14 +131,15 @@ public class AscBossMecha : FightActorStateConector
         {
             do
             {
-                if(await DroppingBomb(cancellationToken)) { break; }
-                if(await Tackle(cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await Util.Aligning(Vector2.zero, Vector2.one, cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await Drop(cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await Fade(cancellationToken)) { break; }
             } while(false);
 
             ongoing = false;
         }
-
-        async UniTask<bool> DroppingBomb(CancellationToken cancellationToken)
+        
+        async UniTask<bool> Drop(CancellationToken cancellationToken)
         {
             while(false /* cond */)
             {
@@ -126,8 +151,9 @@ public class AscBossMecha : FightActorStateConector
             }
             return false;
         }
+
         
-        async UniTask<bool> Tackle(CancellationToken cancellationToken)
+        async UniTask<bool> Fade(CancellationToken cancellationToken)
         {
             while(false /* cond */)
             {
@@ -144,8 +170,11 @@ public class AscBossMecha : FightActorStateConector
     [System.Serializable]
     class SettingUpBombs : ActorState
     {
+        [SerializeField] float bombSetUpGap;
+
         bool ongoing = false;
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        AscBossMecha conector = null; AscBossMecha MechaConnector { get => conector == null ? (conector = Connector as AscBossMecha) : conector; }
 
         protected override bool ShouldCotinue() => ongoing;
         protected override void OnInitialize()
@@ -164,16 +193,24 @@ public class AscBossMecha : FightActorStateConector
         
         async UniTaskVoid Movement(CancellationToken cancellationToken)
         {
+            Rect fieldRect = MechaConnector.fieldRect;
+            Vector2 alignXOrigin = new Vector2(fieldRect.xMin, fieldRect.yMax);
+            Vector2 alignXEnd = alignXOrigin + UnityEngine.Vector2.right * fieldRect.width;
+            Vector2 alignYOrigin = new Vector2(fieldRect.xMax, fieldRect.yMax);
+            Vector2 alignYEnd = alignYOrigin + UnityEngine.Vector2.down * fieldRect.height;
+
             do
             {
-                if(await DroppingBomb(cancellationToken)) { break; }
-                if(await Tackle(cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await Util.Aligning(alignXOrigin, alignXEnd, cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await SetUpBomb(new Vector2(0, -10), fieldRect.height, bombSetUpGap, cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await Util.Aligning(alignYOrigin, alignYEnd, cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await SetUpBomb(new Vector2(-10, 0), fieldRect.width, bombSetUpGap, cancellationToken)) { break; }
             } while(false);
 
             ongoing = false;
         }
 
-        async UniTask<bool> DroppingBomb(CancellationToken cancellationToken)
+        async UniTask<bool> Following(CancellationToken cancellationToken)
         {
             while(false /* cond */)
             {
@@ -186,7 +223,23 @@ public class AscBossMecha : FightActorStateConector
             return false;
         }
         
-        async UniTask<bool> Tackle(CancellationToken cancellationToken)
+        async UniTask<bool> SetUpBomb(Vector2 distance, float setRange, float settingGap, CancellationToken cancellationToken)
+        {
+            while(false /* cond */)
+            {
+                if(cancellationToken.IsCancellationRequested){ return true; }
+                
+                // process
+
+                await UniTask.Yield();
+            }
+            return false;
+        }
+    }
+
+    static class Util
+    {
+        public static async UniTask<bool> Aligning(Vector2 origin, Vector2 end, CancellationToken cancellationToken)
         {
             while(false /* cond */)
             {
