4b78aba748d47f9e9f775d4d48ec2d97a08a459e
iodo_shiba <iodoshiba@gmail.com>
2021-10-10 00:34:32 +0900

    2面ボスの移動を作った

--- Assets/Scripts/Actors/Enemies/Boss Mecha/AscBossMecha.cs
+++ Assets/Scripts/Actors/Enemies/Boss Mecha/AscBossMecha.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx.Async;
+using DG.Tweening;
 using System.Threading;
 using System;
 
 public class AscBossMecha : FightActorStateConector
 {
-    [SerializeField] Rect fieldRect;
+    [SerializeField] Grid fieldGrid;
+    [SerializeField] RectInt fieldRange;
 
     [SerializeField] BossMechaDefault defaultState;
     [SerializeField] BombingTackle bombingTackle;
@@ -16,14 +18,39 @@ public class AscBossMecha : FightActorStateConector
     [SerializeField] SmashedState smashed;
     [SerializeField] BossMechaDead dead;
 
+    ActorState[] ActionOrder => new ActorState[]{bombingTackle, stamping, settingUpBombs};
+
 
     public override ActorState DefaultState => defaultState;
     public override SmashedState Smashed => smashed;
     public override DeadState Dead => dead;
 
+    Rect fieldRect;
+
+    ActorState nextAction = null;
+
     protected override void BuildStateConnection()
     {
-        
+        ConnectStateFromDefault(
+            ()=>{
+                var n = nextAction; 
+                nextAction = null;
+                return n;
+                }
+                );
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        Vector2 fieldOrigin = fieldGrid.transform.position;
+        fieldRect = 
+            new Rect(
+                fieldOrigin + Vector2.Scale(fieldGrid.cellSize, fieldRange.position), 
+                Vector2.Scale(fieldGrid.cellSize, fieldRange.size)
+                );
+
+        ((BossMechaDefault)DefaultState).SetActionOrder(ActionOrder);
     }
 
     public void DropBomb() 
@@ -41,19 +68,56 @@ public class AscBossMecha : FightActorStateConector
 
     }
 
+    void SetNextAction(ActorState actionState)
+    {
+        nextAction = actionState;
+    }
+
     [System.Serializable]
     class BossMechaDefault : DefaultState
     {
+        ActorState[] actionOrder;
+        AscBossMecha ascBossMecha;
+        AscBossMecha AscBossMecha {get => ascBossMecha==null?(ascBossMecha = (AscBossMecha)Connector) : ascBossMecha;}
+        int next = 0;
+        
+        public void SetActionOrder(ActorState[] order)
+        {
+            actionOrder = order;
+            next = 0;
+        }
 
+        protected override void OnInitialize()
+        {
+            ActorState nextAction = getNextAction();
+            AscBossMecha.SetNextAction(nextAction);
+        }
+
+        public ActorState getNextAction()
+        {
+            ActorState nextAction = actionOrder[next];
+            next = (next+1)%actionOrder.Length;
+            return nextAction;
+        }
     }
 
     [System.Serializable]
     class BombingTackle : ActorState
     {
+        [SerializeField] float intervalTime;
+        [SerializeField] float endingTime;
+        [SerializeField] float bombingMoveSpeed;
+        [SerializeField] float bombingMoveCeilGap;
+        [SerializeField] float tackleFloorGap;
+        [SerializeField] float tackleSpeed;
+        [SerializeField] float HorizontalRangeMinShift;
+        [SerializeField] float HorizontalRangeMaxShift;
 
+        AscBossMecha ascBossMecha;
+        AscBossMecha AscBossMecha {get => ascBossMecha==null?(ascBossMecha = (AscBossMecha)Connector) : ascBossMecha;}
+ 
         bool ongoing = false;
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-        AscBossMecha conector = null; AscBossMecha MechaConnector { get => conector == null ? (conector = Connector as AscBossMecha) : conector; }
 
         protected override bool ShouldCotinue() => ongoing;
         protected override void OnInitialize()
@@ -75,7 +139,9 @@ public class AscBossMecha : FightActorStateConector
             do
             {
                 if(cancellationToken.IsCancellationRequested || await DroppingBomb(cancellationToken)) { break; }
+                await UniTask.Delay(TimeSpan.FromSeconds(intervalTime), false, PlayerLoopTiming.Update, cancellationToken);
                 if(cancellationToken.IsCancellationRequested || await Tackle(cancellationToken)) { break; }
+                await UniTask.Delay(TimeSpan.FromSeconds(endingTime), false, PlayerLoopTiming.Update, cancellationToken);
             } while(false);
 
             ongoing = false;
@@ -83,11 +149,22 @@ public class AscBossMecha : FightActorStateConector
 
         async UniTask<bool> DroppingBomb(CancellationToken cancellationToken)
         {
-            while(false /* cond */)
+            float bombingMoveHeight = AscBossMecha.fieldRect.yMax + bombingMoveCeilGap;
+            float beginx = AscBossMecha.fieldRect.xMin + HorizontalRangeMinShift;
+            float endx = AscBossMecha.fieldRect.xMax + HorizontalRangeMaxShift;
+            float time = Mathf.Abs(endx-beginx) / bombingMoveSpeed;
+            float t=0;
+            float z=GameObject.transform.position.z;
+
+            GameObject.transform.position = new Vector3(beginx, bombingMoveHeight, z);
+            while(t<time)
             {
                 if(cancellationToken.IsCancellationRequested){ return true; }
                 
-                // process
+                Vector3 pos = GameObject.transform.position;
+                GameObject.transform.position = new Vector3(Mathf.Lerp(beginx, endx, t/time), bombingMoveHeight, z);
+
+                t += Time.deltaTime;
 
                 await UniTask.Yield();
             }
@@ -96,11 +173,22 @@ public class AscBossMecha : FightActorStateConector
         
         async UniTask<bool> Tackle(CancellationToken cancellationToken)
         {
-            while(false /* cond */)
+            float tackleHeight = AscBossMecha.fieldRect.yMin + tackleFloorGap;
+            float beginx = AscBossMecha.fieldRect.xMax + HorizontalRangeMaxShift;
+            float endx = AscBossMecha.fieldRect.xMin + HorizontalRangeMinShift;
+            float time = Mathf.Abs(endx-beginx) / tackleSpeed;
+            float t=0;
+            float z=GameObject.transform.position.z;
+
+            GameObject.transform.position = new Vector3(beginx, tackleHeight, z);
+            while(t<time)
             {
                 if(cancellationToken.IsCancellationRequested){ return true; }
                 
-                // process
+                Vector3 pos = GameObject.transform.position;
+                GameObject.transform.position = new Vector3(Mathf.Lerp(beginx, endx, t/time), tackleHeight, z);
+
+                t += Time.deltaTime;
 
                 await UniTask.Yield();
             }
@@ -113,7 +201,16 @@ public class AscBossMecha : FightActorStateConector
     {
         bool ongoing = false;
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-        AscBossMecha conector = null; AscBossMecha MechaConnector { get => conector == null ? (conector = Connector as AscBossMecha) : conector; }
+        AscBossMecha ascBossMecha = null; AscBossMecha MechaConnector { get => ascBossMecha == null ? (ascBossMecha = Connector as AscBossMecha) : ascBossMecha; }
+
+
+        [SerializeField] float fadeInCeilShift;
+        [SerializeField] Util.AligningArgs aligningArgs;
+        [SerializeField] float jumpHeight;
+        [SerializeField] float jumpTime;
+        [SerializeField] float dropAccel;
+        [SerializeField] float dropLockTime;
+        [SerializeField] float groundFloorGap;
 
         protected override bool ShouldCotinue() => ongoing;
         protected override void OnInitialize()
@@ -134,7 +231,10 @@ public class AscBossMecha : FightActorStateConector
         {
             do
             {
-                if(cancellationToken.IsCancellationRequested || await Util.Aligning(Vector2.zero, Vector2.one, cancellationToken)) { break; }
+                Util.AligningArgs args = aligningArgs;
+                args.aligningPos = fadeInCeilShift + MechaConnector.fieldRect.yMax;
+
+                if(cancellationToken.IsCancellationRequested || await Util.AligningX(GameObject, ActorManager.PlayerActor.gameObject, args, cancellationToken)) { break; }
                 if(cancellationToken.IsCancellationRequested || await Drop(cancellationToken)) { break; }
                 if(cancellationToken.IsCancellationRequested || await Fade(cancellationToken)) { break; }
             } while(false);
@@ -144,28 +244,57 @@ public class AscBossMecha : FightActorStateConector
         
         async UniTask<bool> Drop(CancellationToken cancellationToken)
         {
-            while(false /* cond */)
-            {
-                if(cancellationToken.IsCancellationRequested){ return true; }
-                
-                // process
 
+            Sequence seq = DOTween.Sequence();
+            seq.Append(GameObject.transform.DOMoveY(GameObject.transform.position.y + jumpHeight, jumpTime).SetEase(Ease.OutQuad));
+            seq.Play();
+            while(seq.IsPlaying())
+            {
+                if(cancellationToken.IsCancellationRequested){ seq.Kill(); return true; }
+                // process
                 await UniTask.Yield();
             }
+            seq.Kill();
+
+            float groundY = MechaConnector.fieldRect.yMin + groundFloorGap;
+            float y0 = GameObject.transform.position.y;
+            float t=0;
+            while(true)
+            {
+                if(cancellationToken.IsCancellationRequested){ return true; }
+                float nexty = y0 - 0.5f*dropAccel*t*t;
+                t += Time.deltaTime;
+                
+                if(nexty < groundY)
+                {
+                    GameObject.transform.position = new Vector3(GameObject.transform.position.x, groundY, GameObject.transform.position.z);
+                    break;
+                }
+                else
+                {
+                    GameObject.transform.position = new Vector3(GameObject.transform.position.x, nexty, GameObject.transform.position.z);
+                }
+                await UniTask.Yield();
+            }
+            await UniTask.Delay(TimeSpan.FromSeconds(dropLockTime), false, PlayerLoopTiming.Update, cancellationToken);
             return false;
         }
 
         
         async UniTask<bool> Fade(CancellationToken cancellationToken)
         {
-            while(false /* cond */)
+            float t=0;
+            Sequence seq = DOTween.Sequence();
+            seq.Append(GameObject.transform.DOMoveY(GameObject.transform.position.y - 5, jumpTime).SetEase(Ease.Linear));
+            seq.Play();
+            while(seq.IsPlaying())
             {
-                if(cancellationToken.IsCancellationRequested){ return true; }
-                
+                if(cancellationToken.IsCancellationRequested){ seq.Kill(); return true; }
                 // process
-
+                t += Time.deltaTime;
                 await UniTask.Yield();
             }
+            seq.Kill();
             return false;
         }
     }
@@ -173,6 +302,18 @@ public class AscBossMecha : FightActorStateConector
     [System.Serializable]
     class SettingUpBombs : ActorState
     {
+        [SerializeField] Util.AligningArgs aligningArgs;
+        [SerializeField] float fadeInCeilShift;
+        [SerializeField] float xAlignRangeMin; 
+        [SerializeField] float xAlignRangeMax;
+        [SerializeField] float fadeInRightWallShift;
+        [SerializeField] float yAlignRangeMin; 
+        [SerializeField] float yAlignRangeMax;
+        [SerializeField] float jumpHeight;
+        [SerializeField] float jumpTime;
+        [SerializeField] float bombSetUpSpeed;
+        [SerializeField] float bombSetUpRangeX;
+        [SerializeField] float bombSetUpRangeY;
         [SerializeField] float bombSetUpGap;
 
         bool ongoing = false;
@@ -197,43 +338,50 @@ public class AscBossMecha : FightActorStateConector
         async UniTaskVoid Movement(CancellationToken cancellationToken)
         {
             Rect fieldRect = MechaConnector.fieldRect;
-            Vector2 alignXOrigin = new Vector2(fieldRect.xMin, fieldRect.yMax);
-            Vector2 alignXEnd = alignXOrigin + UnityEngine.Vector2.right * fieldRect.width;
-            Vector2 alignYOrigin = new Vector2(fieldRect.xMax, fieldRect.yMax);
-            Vector2 alignYEnd = alignYOrigin + UnityEngine.Vector2.down * fieldRect.height;
 
             do
             {
-                if(cancellationToken.IsCancellationRequested || await Util.Aligning(alignXOrigin, alignXEnd, cancellationToken)) { break; }
-                if(cancellationToken.IsCancellationRequested || await SetUpBomb(new Vector2(0, -10), fieldRect.height, bombSetUpGap, cancellationToken)) { break; }
-                if(cancellationToken.IsCancellationRequested || await Util.Aligning(alignYOrigin, alignYEnd, cancellationToken)) { break; }
-                if(cancellationToken.IsCancellationRequested || await SetUpBomb(new Vector2(-10, 0), fieldRect.width, bombSetUpGap, cancellationToken)) { break; }
+                aligningArgs.rangeMin = xAlignRangeMin; 
+                aligningArgs.rangeMax = xAlignRangeMax;
+                aligningArgs.aligningPos = fadeInCeilShift + MechaConnector.fieldRect.yMax;
+                if(cancellationToken.IsCancellationRequested || await Util.AligningX(GameObject, ActorManager.PlayerActor.gameObject, aligningArgs, cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await SetUpBomb(Vector2.down*bombSetUpRangeY, 4, cancellationToken)) { break; }
+                aligningArgs.rangeMin = yAlignRangeMin; 
+                aligningArgs.rangeMax = yAlignRangeMax;
+                aligningArgs.aligningPos = fadeInRightWallShift + MechaConnector.fieldRect.xMax;
+                if(cancellationToken.IsCancellationRequested || await Util.AligningY(GameObject, ActorManager.PlayerActor.gameObject, aligningArgs, cancellationToken)) { break; }
+                if(cancellationToken.IsCancellationRequested || await SetUpBomb(Vector2.left*bombSetUpRangeX, 4, cancellationToken)) { break; }
             } while(false);
 
             ongoing = false;
         }
 
-        async UniTask<bool> Following(CancellationToken cancellationToken)
+        async UniTask<bool> SetUpBomb(Vector2 range, float extraMoveLength, CancellationToken cancellationToken)
         {
-            while(false /* cond */)
-            {
-                if(cancellationToken.IsCancellationRequested){ return true; }
-                
-                // process
+            Vector2 origin = GameObject.transform.position;
 
+            Sequence seq = DOTween.Sequence();
+            seq.Append(GameObject.transform.DOMove(GameObject.transform.position - (Vector3)range.normalized * jumpHeight, jumpTime).SetEase(Ease.OutQuad));
+            seq.Append(GameObject.transform.DOMove(GameObject.transform.position, jumpTime).SetEase(Ease.InQuad));
+            
+            while(seq.IsPlaying())
+            {
+                if(cancellationToken.IsCancellationRequested){ seq.Kill(); return true; }
+                
                 await UniTask.Yield();
             }
-            return false;
-        }
-        
-        async UniTask<bool> SetUpBomb(Vector2 distance, float setRange, float settingGap, CancellationToken cancellationToken)
-        {
-            while(false /* cond */)
+
+            float rangeLength = range.magnitude;
+            Vector2 direction = range/rangeLength;
+            float d = 0;
+            while(d < rangeLength+extraMoveLength)
             {
                 if(cancellationToken.IsCancellationRequested){ return true; }
-                
-                // process
+                float nextd = d+bombSetUpSpeed*Time.deltaTime;
 
+
+                d = nextd;
+                GameObject.transform.position = origin+direction*d;
                 await UniTask.Yield();
             }
             return false;
@@ -242,17 +390,92 @@ public class AscBossMecha : FightActorStateConector
 
     static class Util
     {
-        public static async UniTask<bool> Aligning(Vector2 origin, Vector2 end, CancellationToken cancellationToken)
+        [System.Serializable]
+        public struct AligningArgs
+        { 
+            public float rangeMin; 
+            public float rangeMax;
+            public float aligningPos;
+            public float appearTime;
+            public float alignTime;
+            public float alignSpeed;
+            public float approximationGap;
+        }
+
+        public static async UniTask<bool> AligningX(GameObject obj, GameObject target, AligningArgs args, CancellationToken cancellationToken)
         {
-            while(false /* cond */)
+            obj.transform.position = new Vector3((args.rangeMin + args.rangeMax)/2, args.aligningPos + 5, obj.transform.position.z);
+            float t = 0;
+            while(t < args.appearTime)
             {
                 if(cancellationToken.IsCancellationRequested){ return true; }
                 
-                // process
+                obj.transform.position = new Vector3(obj.transform.position.x, Mathf.Lerp(5, 0, t/args.appearTime)+args.aligningPos, obj.transform.position.z);
+                
+                t += Time.deltaTime;
 
                 await UniTask.Yield();
             }
+
+            t = 0;
+            while(t < args.alignTime)
+            {
+                if(cancellationToken.IsCancellationRequested){ return true; }
+                
+                float targetx = target.transform.position.x;
+                float newx = obj.transform.position.x;
+                if(Mathf.Abs(newx-targetx)>args.approximationGap)
+                {
+                    newx = Mathf.Clamp(newx + Mathf.Sign(targetx-newx)*args.alignSpeed*Time.deltaTime, args.rangeMin, args.rangeMax);
+                }
+                obj.transform.position = new Vector3(newx, args.aligningPos, obj.transform.position.z);
+                
+                t += Time.deltaTime;
+
+                await UniTask.Yield();
+            }
+
             return false;
+
+
+        }
+        
+        public static async UniTask<bool> AligningY(GameObject obj, GameObject target, AligningArgs args, CancellationToken cancellationToken)
+        {
+            obj.transform.position = new Vector3(args.aligningPos + 5, (args.rangeMin + args.rangeMax)/2, obj.transform.position.z);
+            float t = 0;
+            while(t < args.appearTime)
+            {
+                if(cancellationToken.IsCancellationRequested){ return true; }
+                
+                obj.transform.position = new Vector3(args.aligningPos + Mathf.Lerp(5, 0, t/args.appearTime), obj.transform.position.y, obj.transform.position.z);
+                
+                t += Time.deltaTime;
+
+                await UniTask.Yield();
+            }
+
+            t = 0;
+            while(t < args.alignTime)
+            {
+                if(cancellationToken.IsCancellationRequested){ return true; }
+                
+                float targety = target.transform.position.y;
+                float newy = obj.transform.position.y;
+                if(Mathf.Abs(newy-targety)>args.approximationGap)
+                {
+                    newy = Mathf.Clamp(newy + Mathf.Sign(targety-newy)*args.alignSpeed*Time.deltaTime, args.rangeMin, args.rangeMax);
+                }
+                obj.transform.position = new Vector3(args.aligningPos, newy, obj.transform.position.z);
+                
+                t += Time.deltaTime;
+
+                await UniTask.Yield();
+            }
+
+            return false;
+
+
         }
     }
 
