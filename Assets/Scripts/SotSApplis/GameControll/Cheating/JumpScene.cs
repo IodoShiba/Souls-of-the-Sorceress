a6212431e4213c857cad91a911dcc11129ac1e28
IodoShiba <iodoshiba@gmail.com>
2021-11-12 18:00:45 +0900

    2面ボスシーンのカメラのFollowコンポーネントを削除した タイトルで特殊なコマンドを入力すると任意のシーンに飛べるチート機能を実装した

---
+++ Assets/Scripts/SotSApplis/GameControll/Cheating/JumpScene.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SotS.Cheating
+{
+    public class JumpScene : MonoBehaviour
+    {
+        [SerializeField] SceneChanger sceneChanger;
+        [SerializeField] InputField inputField;
+
+        readonly string secretCommand = "forward";
+        int i = 0;
+        bool inputEnabled = false;
+
+        void Start()
+        {
+            inputField.onEndEdit.AddListener((destinationSceneName)=>{sceneChanger.ChangeScene(destinationSceneName);});
+        }
+
+        void Update()
+        {
+            if(inputEnabled){return;}
+
+            if(Input.anyKeyDown)
+            {
+                string inputString = Input.inputString;
+                i = (i < secretCommand.Length && inputString[0] == secretCommand[i]) ? i+1 : 0;
+
+                if(i == secretCommand.Length)
+                {
+                    EnableInput();
+                }
+            }
+        }
+
+        void EnableInput()
+        {
+            inputField.enabled = true;
+            inputField.image.enabled = true;
+            inputField.placeholder.enabled = true;
+
+            inputEnabled = true;
+        }
+    }
+}
