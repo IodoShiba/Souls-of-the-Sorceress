390be347458e248b4b9df456d9184c5059318feb
iodoshiba_pc <iodoshiba@gmail.com>
2022-03-24 00:10:27 +0900

    ステージの各シーンに元から存在する敵の数等のメタデータを記録するSceneMetaDataと、それを編集するウィンドウを作成した

--- Assets/Scripts/Editor/Stage/StageMetaDataEditor.cs
+++ Assets/Scripts/Editor/Stage/StageMetaDataEditor.cs
@@ -1,125 +1,192 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UniRx;
-using UnityEditor;
-using System.Linq;
 using UnityEngine.SceneManagement;
-using System;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
 
 [CustomEditor(typeof(StageMetaData))]
 public class StageMetaDataEditor : Editor
 {
-    GameObject[] enemysGameObjects = new GameObject[0];
-    Vector2 scrpos = Vector2.zero;
-    bool nullObjectContained = false;
-    string currentScenePath = "";
-
-    public override async void OnInspectorGUI()
-    {   
-        currentScenePath = SceneManager.GetActiveScene().path;
-
-        ShowCurrentSceneStatus();
-
-        EditorGUILayout.LabelField("wahh");
-
-        EditorGUILayout.Separator();
-
-        EditorGUILayout.LabelField("Entries");
-        var sceneEntry = serializedObject.FindProperty("sceneEntry");
-        var entryCount = sceneEntry.arraySize;
-        for (int i=0; i<entryCount; ++i)
-        {
-            var elem = sceneEntry.GetArrayElementAtIndex(i);
-            EditorGUILayout.PropertyField(elem, true);
-        }
-
-        serializedObject.ApplyModifiedProperties();
-    }
-
-    void ShowCurrentSceneStatus()
+    public override void OnInspectorGUI()
     {
-        if (GUILayout.Button("Search Enemies In Current Scene"))
+        if(GUILayout.Button("Open Editor Window"))
         {
-            enemysGameObjects = Resources.FindObjectsOfTypeAll<GameObject>()
-                .Where(obj => obj.scene.IsValid() && obj.TryGetComponent<Enemy>(out _))
-                .ToArray();
-            nullObjectContained = false;
+            EditorWindow.GetWindow<StageMetaDataWindow>("Stage Meta Data Window");
         }
+        EditorGUILayout.HelpBox("Due to containing process across scenes,\nediting must be done in special window \nwhich is scene independent workspace.", MessageType.Info);
 
-
-        EditorGUILayout.LabelField("Current Scene Path: ", currentScenePath);
-        EditorGUILayout.LabelField("Enemy counts: ", enemysGameObjects.Length.ToString());
-        if(nullObjectContained)
-        {
-            EditorGUILayout.HelpBox("Null GameObject is contained in GameObject List.\nRetry search.", MessageType.Warning);
-        }
-        using(new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
-        using(var scrscope = new EditorGUILayout.ScrollViewScope(scrpos))
-        {
-            for(int i=0; i<enemysGameObjects.Length; ++i)
-            {
-                if (enemysGameObjects[i] == null)
-                {
-                    EditorGUILayout.LabelField("-- null --");
-                    continue;
-                }
-
-                EditorGUILayout.LabelField(enemysGameObjects[i].name);
-            }
-
-            scrpos = scrscope.scrollPosition;
-        }
-
-        if(GUILayout.Button("Write This Data"))
-        {
-            WriteSceneMetaData();
-        }
-        if(GUILayout.Button("Delete Missing Entry"))
-        {
-            DeleteMissingEntry();
-        }
-    }
-
-    void WriteSceneMetaData()
-    {
-        var sceneEntry = serializedObject.FindProperty("sceneEntry");
-        var entryCount = sceneEntry.arraySize;
-        for (int i=0; i<entryCount; ++i)
-        {
-            var elem = sceneEntry.GetArrayElementAtIndex(i);
-            if(currentScenePath == elem.FindPropertyRelative("scenePath").stringValue)
-            {
-                WriteEntryProperty(elem);
-                return;
-            }
-        }
-        sceneEntry.InsertArrayElementAtIndex(sceneEntry.arraySize);
-        var newelem = sceneEntry.GetArrayElementAtIndex(sceneEntry.arraySize-1);
-        WriteEntryProperty(newelem);
-    }
-
-    void WriteEntryProperty(SerializedProperty entry)
-    {
-        entry.FindPropertyRelative("scenePath").stringValue = currentScenePath;
-        entry.FindPropertyRelative("enemyCount").intValue = enemysGameObjects.Length;
-    }
-
-    async void DeleteMissingEntry()
-    {
-        var sceneEntry = serializedObject.FindProperty("sceneEntry");
-        var entryCount = sceneEntry.arraySize;
-        for(int i=sceneEntry.arraySize-1; 0<=i; --i)
-        {
-            var scenePath = sceneEntry.GetArrayElementAtIndex(i)
-                .FindPropertyRelative("scenePath")
-                .stringValue;
-
-            scenePath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), scenePath.Replace('/', '\\'));
-
-            if(!System.IO.File.Exists(scenePath))
-            {
-                sceneEntry.DeleteArrayElementAtIndex(i);
-            }
-        }
+        EditorGUILayout.Space();
+        
+        EditorGUI.BeginDisabledGroup(true);
+        base.OnInspectorGUI();
+        EditorGUI.BeginDisabledGroup(false);
     }
 }
+// [CustomEditor(typeof(StageMetaData))]
+// public class StageMetaDataEditor : Editor
+// {
+//     ScriptableObject stageMetaData;
+
+//     public override void OnInspectorGUI()
+//     {   
+//         var currentScenePath = SceneManager.GetActiveScene().path;
+
+//         // ShowCurrentSceneStatus();
+
+//         EditorGUILayout.LabelField("wahh");
+
+//         EditorGUILayout.Separator();
+
+//         stageMetaData = EditorGUILayout.ObjectField(stageMetaData, typeof(StageMetaData), false) as StageMetaData;
+
+//         EditorGUILayout.LabelField("Stage Entries");
+//         var stageEntries = serializedObject.FindProperty("stageEntries");
+//         var stageEntriesCount = stageEntries.arraySize;
+//         for (int i=0; i<stageEntriesCount; ++i)
+//         {
+//             var stageElem = stageEntries.GetArrayElementAtIndex(i);
+            
+//             using (new EditorGUILayout.HorizontalScope(EditorStyles.helpBox))
+//             {
+//                 var doDeleteStage = GUILayout.Button("x", GUILayout.Width(24));
+                
+//                 EditorGUILayout.BeginVertical();
+//                 var stage = stageElem.FindPropertyRelative("stage");
+
+//                 StageMetaData.Stage e = (StageMetaData.Stage)EditorGUILayout.EnumPopup((StageMetaData.Stage)stage.intValue);
+//                 stage.intValue = (int)e;
+                
+
+//                 var sceneEntries = stageElem.FindPropertyRelative("sceneEntries");
+//                 var sceneEntriesCount = sceneEntries.arraySize;
+//                 for(int j=0;j<sceneEntriesCount;++j)
+//                 {
+//                     var sceneElem = sceneEntries.GetArrayElementAtIndex(j);
+                    
+//                     using(new EditorGUILayout.HorizontalScope(EditorStyles.helpBox))
+//                     {
+//                         var doDeleteScene = GUILayout.Button("x", GUILayout.Width(24));
+                        
+//                         EditorGUILayout.BeginVertical();
+//                         EditorGUILayout.PropertyField(sceneElem, true);
+//                         if (GUILayout.Button("Update Meta Data"))
+//                         {
+//                             var currentOpenScenePath = EditorSceneManager.GetActiveScene().path;
+//                             UpdateSceneMetaData(sceneElem);
+//                             EditorSceneManager.OpenScene(currentOpenScenePath);
+
+//                         }
+
+//                         EditorGUILayout.EndVertical();
+
+//                         if(doDeleteScene)
+//                         {
+//                             sceneEntries.DeleteArrayElementAtIndex(j);
+//                             break;
+//                         }
+//                     }
+//                 }
+//                 if(GUILayout.Button("Add New Scene Entry"))
+//                 {
+//                     sceneEntries.InsertArrayElementAtIndex(sceneEntriesCount);
+//                 }
+//                 if(GUILayout.Button("Update Meta Data of All Scenes in This Stage"))
+//                 {
+//                     var currentOpenScenePath = EditorSceneManager.GetActiveScene().path;
+//                     for(int j=0;j<sceneEntriesCount;++j)
+//                     {
+//                         var sceneElem = sceneEntries.GetArrayElementAtIndex(j);
+//                         UpdateSceneMetaData(sceneElem);
+//                     }
+//                     EditorSceneManager.OpenScene(currentOpenScenePath);
+//                 }
+//                 EditorGUILayout.EndVertical();
+
+//                 if(doDeleteStage)
+//                 {
+//                     stageEntries.DeleteArrayElementAtIndex(i);
+//                     break;
+//                 }
+//             }
+//         }
+//         if(GUILayout.Button("Add New Stage Entry"))
+//         {
+//             stageEntries.InsertArrayElementAtIndex(stageEntriesCount);
+//         }
+//         if(GUILayout.Button("Update Meta Data of All Scenes"))
+//         {
+//             var currentOpenScenePath = EditorSceneManager.GetActiveScene().path;
+//             for(int i=0;i<stageEntriesCount;++i)
+//             {
+//                 var sceneEntries = stageEntries.GetArrayElementAtIndex(i).FindPropertyRelative("SceneEntries");
+//                 var sceneEntriesCount = sceneEntries.arraySize;
+//                 for(int j=0; j<sceneEntriesCount; ++j)
+//                 {
+//                     var sceneElem = sceneEntries.GetArrayElementAtIndex(j);
+//                     UpdateSceneMetaData(sceneElem);
+//                 }
+//             }
+//             EditorSceneManager.OpenScene(currentOpenScenePath);
+//         }
+
+//         serializedObject.ApplyModifiedProperties();
+//     }
+
+//     void UpdateSceneMetaData(SerializedProperty sceneEntryProp)
+//     {
+//         var scene = sceneEntryProp.FindPropertyRelative("scene");
+//         var sceneName = scene.FindPropertyRelative("m_SceneName").stringValue;
+//         var enemyCount = sceneEntryProp.FindPropertyRelative("enemyCount");
+
+//         if (string.IsNullOrEmpty(sceneName))
+//         {
+//             return;
+//         }
+
+//         string scenePath = string.Empty;
+//         for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+//         {
+//             EditorBuildSettingsScene ebsscene = EditorBuildSettings.scenes[i];
+//             if (ebsscene.path.IndexOf(sceneName) != -1)
+//             {
+//                 scenePath = ebsscene.path;
+
+//                 break;
+//             }
+//         }
+//         if (string.IsNullOrEmpty(scenePath)){ return; }
+        
+//         Scene sceneStruct = EditorSceneManager.OpenScene(scenePath);
+//         GameObject[] roots = sceneStruct.GetRootGameObjects();
+//         int enemyCountValue = 0;
+//         for (int i=0; i<roots.Length; ++i)
+//         {
+//             enemyCountValue += roots[i].GetComponentsInChildren<Enemy>(true).Length;
+//         }
+//         enemyCount.intValue = enemyCountValue;
+
+//     }
+
+//     void DeleteMissingEntry()
+//     {
+//         var sceneEntry = serializedObject.FindProperty("sceneEntry");
+//         var entryCount = sceneEntry.arraySize;
+//         for(int i=sceneEntry.arraySize-1; 0<=i; --i)
+//         {
+//             var scenePath = sceneEntry.GetArrayElementAtIndex(i)
+//                 .FindPropertyRelative("scenePath")
+//                 .stringValue;
+
+//             scenePath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), scenePath.Replace('/', '\\'));
+
+//             if(!System.IO.File.Exists(scenePath))
+//             {
+//                 sceneEntry.DeleteArrayElementAtIndex(i);
+//             }
+//         }
+//     }
+// }
--- Assets/Scripts/Scriptables/StageMetaData.cs
+++ Assets/Scripts/Scriptables/StageMetaData.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
+using UnityEngine;
+using UniRx;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+using UnityEditor;
+#endif
 
 [CreateAssetMenu(menuName = "StageMetaData")]
 public class StageMetaData : ScriptableObject
 {
-    public const string path = "Assets/StageMetaData.asset";
+    public const string path = "Assets/Resources/SotSApplis/Stage Meta Data.asset";
 
     public enum Stage
     {
         StageEX = -1,
-        Stage0 = 1,
+        Stage0 = 0,
         Stage1 = 1,
         Stage2 = 2,
         Stage3 = 3,
@@ -20,20 +27,193 @@ public class StageMetaData : ScriptableObject
     [System.Serializable]
     public struct SceneEntry
     {
-        [DisabledField] public string scenePath;
-        public Stage stageBelongTo;
+        public SceneAsset scene;
         [DisabledField] public int enemyCount;
     }
 
-    [SerializeField] List<SceneEntry> sceneEntry = new List<SceneEntry>();
-
-    public bool TryGetEntry(string scenePath, out SceneEntry outSceneEntry)
+    [System.Serializable]
+    public struct StageEntry
     {
-        IEnumerable<SceneEntry> match = sceneEntry.Where(e => e.scenePath == scenePath);
-        if(match.Count() == 0){ outSceneEntry = default; return false; }
+        public Stage stage;
+        public List<SceneEntry> sceneEntries;
 
-        outSceneEntry = match.FirstOrDefault();
-        return true;
+        public StageEntry(Stage stage = default)
+        {
+            this.stage = stage;
+            sceneEntries = new List<SceneEntry>() {default};
+        }
     }
 
+    // [SerializeField] List<SceneEntry> sceneEntry = new List<SceneEntry>();
+    [DisabledField, SerializeField] List<StageEntry> stageEntries = new List<StageEntry>();
+
+
+
+
+#if UNITY_EDITOR
+    public class StageMetaDataWindowBase : EditorWindow
+    {
+        bool initialized = false;
+        StageMetaData target;
+        Vector2 scrpos = Vector2.zero;
+
+        async void OnEnable()
+        {
+            if(initialized) {return;}
+
+            var t = AssetDatabase.LoadAssetAtPath(path, typeof(StageMetaData));
+            Debug.Assert(t != null);
+            target = t as StageMetaData;
+
+            initialized = true;
+        }
+
+        public void OnGUI()
+        {   
+            if(target == null){return;}
+
+            using(var scrscope = new EditorGUILayout.ScrollViewScope(scrpos))
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Stage Entries");
+                for (int i=0; i<target.stageEntries.Count; ++i)
+                {
+                    var stageEntry = target.stageEntries[i];
+                    EditorGUILayout.Space();
+                    
+                    using (new EditorGUILayout.HorizontalScope(EditorStyles.helpBox))
+                    {
+                        var doDeleteStage = GUILayout.Button("x", GUILayout.Width(24));
+                        
+                        EditorGUILayout.BeginVertical();
+
+                        stageEntry.stage = (StageMetaData.Stage)EditorGUILayout.EnumPopup("Stage: ", target.stageEntries[i].stage);
+
+                        if (stageEntry.sceneEntries == null)
+                        {
+                            stageEntry.sceneEntries = new List<SceneEntry>();
+                        }
+
+                        for(int j=0;j<stageEntry.sceneEntries.Count;++j)
+                        {
+                            var sceneEntry = stageEntry.sceneEntries[j];
+                            
+                            using(new EditorGUILayout.HorizontalScope(EditorStyles.helpBox))
+                            {
+                                var doDeleteScene = GUILayout.Button("x", GUILayout.Width(24));
+                                
+                                EditorGUILayout.BeginVertical();
+                                
+                                sceneEntry.scene = EditorGUILayout.ObjectField("ScenePath", sceneEntry.scene, typeof(SceneAsset), false) as SceneAsset;
+                                EditorGUILayout.LabelField("Enemy Count: ", sceneEntry.enemyCount.ToString());
+                                if (GUILayout.Button("Update Meta Data"))
+                                {
+                                    var currentOpenScenePath = EditorSceneManager.GetActiveScene().path;
+                                    UpdateSceneMetaData(ref sceneEntry);
+                                    EditorSceneManager.OpenScene(currentOpenScenePath);
+                                }
+
+                                EditorGUILayout.EndVertical();
+
+                                if(doDeleteScene)
+                                {
+                                    stageEntry.sceneEntries.RemoveAt(j);
+                                    break;
+                                }
+                            }
+
+                            stageEntry.sceneEntries[j] = sceneEntry;
+                        }
+                        EditorGUILayout.Space();
+                        if(GUILayout.Button("Add New Scene Entry"))
+                        {
+                            stageEntry.sceneEntries.Add(default);
+                        }
+                        if(GUILayout.Button("Update Meta Data of All Scenes in This Stage"))
+                        {
+                            var currentOpenScenePath = EditorSceneManager.GetActiveScene().path;
+                            for(int j=0;j<stageEntry.sceneEntries.Count;++j)
+                            {
+                                var sceneEntry = stageEntry.sceneEntries[j];
+                                UpdateSceneMetaData(ref sceneEntry);
+                                stageEntry.sceneEntries[j] = sceneEntry;
+                            }
+                            EditorSceneManager.OpenScene(currentOpenScenePath);
+                        }
+                        EditorGUILayout.EndVertical();
+
+                        if(doDeleteStage)
+                        {
+                            target.stageEntries.RemoveAt(i);
+                            break;
+                        }
+
+                    }
+                    target.stageEntries[i] = stageEntry;
+
+                    EditorGUILayout.Space();
+                }
+                EditorGUILayout.Space();
+                if(GUILayout.Button("Add New Stage Entry"))
+                {
+                    if(target.stageEntries == null)
+                    {
+                        target.stageEntries = new List<StageEntry>();
+                    }
+                    target.stageEntries.Add(new StageEntry());
+                }
+                if(GUILayout.Button("Update Meta Data of All Scenes"))
+                {
+                    var currentOpenScenePath = EditorSceneManager.GetActiveScene().path;
+                    for(int i=0;i<target.stageEntries.Count;++i)
+                    {
+                        var sceneEntries = target.stageEntries[i].sceneEntries;
+                        for(int j=0; j<sceneEntries.Count; ++j)
+                        {
+                            var sceneEntry = sceneEntries[j];
+                            UpdateSceneMetaData(ref sceneEntry);
+                            sceneEntries[j] = sceneEntry;
+                        }
+                    }
+                    EditorSceneManager.OpenScene(currentOpenScenePath);
+                }
+
+                scrpos = scrscope.scrollPosition;
+            }
+        }
+
+        void UpdateSceneMetaData(ref SceneEntry sceneEntry)
+        {
+            var sceneName = sceneEntry.scene.name;
+
+            if (sceneEntry.scene == null)
+            {
+                return;
+            }
+
+            string scenePath = string.Empty;
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                EditorBuildSettingsScene ebsscene = EditorBuildSettings.scenes[i];
+                if (ebsscene.path.IndexOf(sceneName) != -1)
+                {
+                    scenePath = ebsscene.path;
+
+                    break;
+                }
+            }
+            if (string.IsNullOrEmpty(scenePath)){ return; }
+            
+            Scene sceneStruct = EditorSceneManager.OpenScene(scenePath);
+            GameObject[] roots = sceneStruct.GetRootGameObjects();
+            int enemyCountValue = 0;
+            for (int i=0; i<roots.Length; ++i)
+            {
+                enemyCountValue += roots[i].GetComponentsInChildren<Enemy>(true).Length;
+            }
+            sceneEntry.enemyCount = enemyCountValue;
+
+        }
+    }
+#endif
 }
