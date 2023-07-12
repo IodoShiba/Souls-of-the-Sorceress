6952085bafad4aabc6d8565ec470fcc1592fdb61
iodoshiba_pc <iodoshiba@gmail.com>
2023-07-12 22:38:42 +0900

    画面遷移に伴うメニュー要素の表示・非表示の仕様を調整した 重複を赤くハイライトするようにし、警告メッセージを表示するようにした

---
+++ Assets/Scripts/Utilities/TextConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextConverter : MonoBehaviour
+{
+    [System.Serializable]
+    public class UnityEventTextConversion : UnityEvent<string> { }
+
+    [System.Serializable]
+    struct TextPair
+    {
+        public string textSource;
+        public string textDestination;
+    }
+
+    [SerializeField] TextPair[] textPairs;
+    [SerializeField] UnityEventTextConversion eventTextConverted;
+
+    Dictionary<string, string> textPairsDictionary = new Dictionary<string, string>(16);
+
+
+    private void Awake()
+    {
+        foreach(var pair in textPairs)
+        {
+            textPairsDictionary[pair.textSource] = pair.textDestination;
+        }
+    }
+
+    public void SendTextEvent(string sourceText)
+    {
+        string dest;
+        string textConverted = textPairsDictionary.TryGetValue(sourceText, out dest) ? dest : sourceText;
+
+        eventTextConverted.Invoke(textConverted);
+    }
+}
