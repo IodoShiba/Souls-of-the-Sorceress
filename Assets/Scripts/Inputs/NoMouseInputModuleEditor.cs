d66f21d2710f693b199365d318ce0357a9ebf0bc
iodoshiba_pc <iodoshiba@gmail.com>
2023-10-15 14:59:55 +0900

    NoMouseInputModuleEditorをコメントアウト

--- Assets/Scripts/Inputs/NoMouseInputModuleEditor.cs
+++ Assets/Scripts/Inputs/NoMouseInputModuleEditor.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 namespace Assets.Scripts.Inputs
 {
-    [CustomEditor(typeof(NoMouseInputModule))]
-    public class NoMouseInputModuleEditor : UnityEditor.Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-        }
-    }
+    // [CustomEditor(typeof(NoMouseInputModule))]
+    // public class NoMouseInputModuleEditor : UnityEditor.Editor
+    // {
+    //     public override void OnInspectorGUI()
+    //     {
+    //         base.OnInspectorGUI();
+    //     }
+    // }
 }
